dff934a2c5a244108b038114cc530e3ffa179f36
Muhammad Talha <talhahanfsd@gmail.com>
2023-02-26 22:38:58 +0500

    Change Video Component

--- src/modules/Video/index.js
+++ src/modules/Video/index.js
@@ -17,9 +17,9 @@ function Video() {
           className="absolute top-0 left-0 bottom-0 right-0 w-full h-full"
           src="https://www.youtube-nocookie.com/embed/V-_O7nl0Ii0"
           title="Video"
-          frameborder="0"
+          frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
+          allowFullScreen
         ></iframe>
       </div>
     </div>
