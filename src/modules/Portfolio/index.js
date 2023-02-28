f9c0f3a6eadae0a235f726a82ce7c277665cf3aa
Muhammad Talha <talhahanfsd@gmail.com>
2023-02-28 18:38:29 +0500

    Design Carousel

--- src/modules/Portfolio/index.js
+++ src/modules/Portfolio/index.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
+import zigzag from "../../assets/vectorZigZag.png";
+import circle from "../../assets/vectorCircle.png";
+import Carousel from "react-elastic-carousel";
+import Item from "./Item";
+import interior from '../../assets/portfolioInterior.png'
+import digi from '../../assets/portfolioDigi.png'
+import squid from '../../assets/portfolioSquid.png'
+
+const breakPoints = [
+  { width: 1, itemsToShow: 1 },
+  // { width: 550, itemsToShow: 2 },
+  { width: 768, itemsToShow: 3 },
+];
 
 function Portfolio() {
   return (
-    <div>Portfolio</div>
-  )
+    <div className="relative">
+      <div className="relative">
+        <img
+          src={circle}
+          alt=""
+          className="absolute bottom-0 right-0 h-[42px] md:h-[60px]  rotate-[-12deg]"
+        />
+        <h3 className="py-10 lg:py-20 text-[2.5rem] md:text-[3rem] lg:text-[4rem] font-mont font-bold leading-snug w-[95%]">
+          Our Awesome Portofolio
+        </h3>
+      </div>
+      <div>
+      <Carousel breakPoints={breakPoints} className="">
+        <Item><img src={interior} alt="" /></Item>
+        <Item><img src={digi} alt="" /></Item>
+        <Item><img src={squid} alt="" /></Item>
+        <Item><img src={interior} alt="" /></Item>
+        <Item><img src={digi} alt="" /></Item>
+        <Item><img src={squid} alt="" /></Item>
+        <Item><img src={interior} alt="" /></Item>
+        <Item><img src={digi} alt="" /></Item>
+        <Item><img src={squid} alt="" /></Item>
+        </Carousel>
+      </div>
+      <img
+        src={zigzag}
+        alt=""
+        className="absolute bottom-0 left-0 h-[18px] md:h-[28px]"
+      />
+    </div>
+  );
 }
 
-export default Portfolio
+export default Portfolio;
